0479004c7004f76ce77b4dc2079a1bf9cf843119
Jan Meppe <janmeppe@hotmail.com>
2020-06-19 10:02:02 +0200

    Add readme

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button, ScrollView } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [enteredGoal, setEnteredGoal] = useState('');
+    const [courseGoals, setCourseGoals] = useState([]);
+
+    const goalInputHandler = (enteredText) => {
+        setEnteredGoal(enteredText);
+    }
+
+    const addGoalHandler = () => {
+      setCourseGoals([...courseGoals, enteredGoal])
+    }
+
+    return (
+        <View style={styles.screen}>
+            <View style={styles.inputContainer}>
+                <TextInput placeholder='Course Goal' style={styles.input} onChangeText={goalInputHandler} value={enteredGoal}/>
+                <Button title='ADD' onPress={addGoalHandler}/>
+            </View>
+            <ScrollView>
+              {courseGoals.map((goal) => <View style={styles.listItem}><Text key={goal}>{goal}</Text></View>)}
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    screen: {
+        padding: 50,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        width: '80%',
+        borderColor: 'black',
+        borderWidth: 1,
+        padding: 10,
+    },
+    listItem: {
+      padding: 10,
+      marginVertical: 10,
+      backgroundColor: '#ccc',
+      borderWidth: 1,
+      borderColor: 'black'
+    }
 });
---
+++ README.md
@@ -0,0 +1,31 @@
+# React native course
+
+Hi! I'm currently following the course [React Native - The Practical Guide 2020](https://www.udemy.com/course/react-native-the-practical-guide/) and these are my notes and code.
+
+## Requirements
+
+To run it on MacOS you need: 
+
+* NodeJS
+* Yarn
+* Expo-cli
+
+## Usage
+
+Clone the repo and run
+
+```
+npm start
+```
+
+Then open a simulator and in Expo DevTools click on `run on iOS simulator`. 
+
+## What the app looks like
+
+### v1.0.0
+
+![](/doc/img1.png)
+
+## Links
+
+* [Link to course](https://www.udemy.com/course/react-native-the-practical-guide/)
